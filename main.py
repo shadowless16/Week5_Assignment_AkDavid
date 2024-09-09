6894538fa2279f6030ed98b742e2ee5a00c4a888
shadowless16 <akdavid4real@gmail.com>
2024-09-09 11:39:35 +0100

    Completed Task 1: Dice Roller Simulation

---
+++ main.py
@@ -0,0 +1,15 @@
+# Part 1: Randomization (30 points) 
+# Task 1: Dice Roller Simulation (10 points) 
+# ● Write a Python program that simulates rolling two six-sided dice. ● The program should generate two random integers between 1 and 6. ● Add the results together and print the total. 
+import random
+
+print("Welcome to the Randomization application")
+
+roll_dice1 = print(random.randint(1, 6))
+
+roll_dice2 = print(random.randint(1, 6))
+
+print("Total:", random.randint(1, 6) + random.randint(1, 6))
+
+# print("Rolling dice:",  roll_dice1 + roll_dice2)
+
